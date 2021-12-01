8be51960ed7c8dfba1580b36dc68136a04ea8f3a
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-01 17:48:36 +0545

    first commit

---
+++ Rollingdice.py
@@ -0,0 +1,16 @@
+from tkinter import*
+
+
+#main, window, size, add title
+root = Tk()
+root.title("Rolling Dice")
+root.geometry("500x500")
+
+#label to display dice
+label = Tk(root, text="", font=("lucinda", 260))
+
+
+
+
+
+root.mainloop()
